fd66b684084f8e453eb8e1b823892316a6ec9b34
jud1thdev <8w8u8@naver.com>
2025-10-01 17:22:01 +0900

    fix: JPA 리포지토리 인식 문제 컴포넌트 스캔 범위 설정으로 해결

--- qbit-api-app/src/main/java/com/curihous/qbit/api/QbitApiApplication.java
+++ qbit-api-app/src/main/java/com/curihous/qbit/api/QbitApiApplication.java
@@ -2,8 +2,12 @@ package com.curihous.qbit.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.curihous.qbit")
+@EnableJpaRepositories(basePackages = "com.curihous.qbit.domain")
+@EntityScan(basePackages = "com.curihous.qbit.domain")
 public class QbitApiApplication {
 
     public static void main(String[] args) {
