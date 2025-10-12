8b9b5b531b852b90c714146db68b0c53434e5b56
jud1thdev <8w8u8@naver.com>
2025-10-13 06:18:47 +0900

    fix: 순환참조 문제

--- qbit-realtime-app/src/main/java/com/curihous/qbit/realtime/config/FinnhubWebSocketConfig.java
+++ qbit-realtime-app/src/main/java/com/curihous/qbit/realtime/config/FinnhubWebSocketConfig.java
@@ -1,8 +1,6 @@
 package com.curihous.qbit.realtime.config;
 
 import com.curihous.qbit.realtime.websocket.FinnhubWebSocketManager;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +8,6 @@ import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
 @Configuration
-@RequiredArgsConstructor
 public class FinnhubWebSocketConfig {
 
     @Value("${finnhub.api.websocket-url}")
@@ -19,22 +16,15 @@ public class FinnhubWebSocketConfig {
     @Value("${finnhub.api.key}")
     private String apiKey;
 
-    private final FinnhubWebSocketManager finnhubWebSocketManager;
-
     @Bean
     public WebSocketClient webSocketClient() {
         return new StandardWebSocketClient();
     }
 
     @Bean
-    public FinnhubWebSocketManager finnhubWebSocketManager() {
-        return new FinnhubWebSocketManager(websocketUrl, apiKey);
-    }
-
-    // 애플리케이션 시작 시 WebSocket 초기화
-    @PostConstruct
-    public void initializeWebSocket() {
-        WebSocketClient client = webSocketClient();
-        finnhubWebSocketManager.initialize(client);
+    public FinnhubWebSocketManager finnhubWebSocketManager(WebSocketClient webSocketClient) {
+        FinnhubWebSocketManager manager = new FinnhubWebSocketManager(websocketUrl, apiKey);
+        manager.initialize(webSocketClient);  
+        return manager;
     }
 }
