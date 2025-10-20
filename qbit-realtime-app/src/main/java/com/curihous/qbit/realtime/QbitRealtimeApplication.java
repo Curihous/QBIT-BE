72797b632fd34cc8d5ce202d74cc5618f8800a29
jud1thdev <8w8u8@naver.com>
2025-10-20 21:02:16 +0900

    hotfix: qbit-infra 컴포넌트 스캔

--- qbit-realtime-app/src/main/java/com/curihous/qbit/realtime/QbitRealtimeApplication.java
+++ qbit-realtime-app/src/main/java/com/curihous/qbit/realtime/QbitRealtimeApplication.java
@@ -8,7 +8,8 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 @SpringBootApplication(
     scanBasePackages = {
         "com.curihous.qbit.realtime",
-        "com.curihous.qbit.common"
+        "com.curihous.qbit.common",
+        "com.curihous.qbit.infra"
     },
     exclude = {
         DataSourceAutoConfiguration.class,
