b3463e04484003f832e9f341a510c69d93cbbdb9
jud1thdev <8w8u8@naver.com>
2025-10-31 16:47:09 +0900

    fix: User 로그인 시 주문 동기화 발행

--- qbit-realtime-app/src/main/java/com/curihous/qbit/realtime/consumer/LoginOrderSyncConsumer.java
+++ qbit-realtime-app/src/main/java/com/curihous/qbit/realtime/consumer/LoginOrderSyncConsumer.java
@@ -27,7 +27,9 @@ public class LoginOrderSyncConsumer implements StreamListener<String, MapRecord<
             
             // 값에서 JSON 따옴표 제거
             String userIdStr = fields.get("userId").replaceAll("^\"|\"$", "");
-            String accessToken = fields.get("accessToken").replaceAll("^\"|\"$", "");
+            String accessToken = fields.get("accessToken") != null 
+                    ? fields.get("accessToken").replaceAll("^\"|\"$", "") 
+                    : "";
             
             Long userId = Long.parseLong(userIdStr);
             
@@ -36,9 +38,12 @@ public class LoginOrderSyncConsumer implements StreamListener<String, MapRecord<
             
             // Alpaca WebSocket 구독
             if (accessToken != null && !accessToken.isEmpty()) {
+                // 이벤트에 토큰이 있으면 subscribe() 호출 (토큰 저장 + 구독)
                 alpacaTradeUpdatesManager.subscribe(userId, accessToken);
             } else {
-                log.warn("Access Token이 없어 Alpaca 구독을 건너뜁니다: userId={}", userId);
+                // 이벤트에 토큰이 없어도 저장된 토큰으로 구독 시도
+                log.info("이벤트에 Access Token이 없음. 저장된 토큰으로 구독 시도: userId={}", userId);
+                alpacaTradeUpdatesManager.subscribeIfHasToken(userId);
             }
             
         } catch (Exception e) {
