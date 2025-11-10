7b1a9a340dacbbaa871dfdb2eda5055df6ef2de0
jud1thdev <8w8u8@naver.com>
2025-11-10 20:35:51 +0900

    remove: daily_assets

--- qbit-domain/src/main/java/com/curihous/qbit/domain/asset/service/DailyAssetService.java
+++
@@ -1,8 +0,0 @@
-package com.curihous.qbit.domain.asset.service;
-
-import com.curihous.qbit.domain.asset.entity.DailyAsset;
-import org.springframework.stereotype.Service;
-
-@Service
-public class DailyAssetService {
-}
