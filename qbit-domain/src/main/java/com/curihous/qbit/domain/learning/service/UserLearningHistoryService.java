c6d4ec157fdf030736a05bec446f8973a5362a48
jud1thdev <8w8u8@naver.com>
2025-11-10 15:28:41 +0900

    remove: journal, user_learning_histories

--- qbit-domain/src/main/java/com/curihous/qbit/domain/learning/service/UserLearningHistoryService.java
+++
@@ -1,8 +0,0 @@
-package com.curihous.qbit.domain.learning.service;
-
-import com.curihous.qbit.domain.learning.entity.UserLearningHistory;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserLearningHistoryService {
-}
