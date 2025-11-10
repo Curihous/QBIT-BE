fea4d4f6b05352ed783ad80db97fdeac3f7f6700
jud1thdev <8w8u8@naver.com>
2025-11-10 15:29:33 +0900

    remove: tradeReposrt

--- qbit-domain/src/main/java/com/curihous/qbit/domain/tradereport/repository/TradeReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.curihous.qbit.domain.tradereport.repository;
-
-import com.curihous.qbit.domain.tradereport.entity.TradeReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TradeReportRepository extends JpaRepository<TradeReport, Long> {
-}
