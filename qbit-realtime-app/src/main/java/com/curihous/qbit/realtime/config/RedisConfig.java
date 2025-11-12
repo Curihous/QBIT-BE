3236f20b406b1dc1dc0e0d638adb29a6548d0b52
jud1thdev <8w8u8@naver.com>
2025-11-12 23:13:54 +0900

    refactor: RedisSerializerConfig 빈 재사용

--- qbit-realtime-app/src/main/java/com/curihous/qbit/realtime/config/RedisConfig.java
+++ qbit-realtime-app/src/main/java/com/curihous/qbit/realtime/config/RedisConfig.java
@@ -1,16 +1,11 @@
 package com.curihous.qbit.realtime.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 /**
  * Redis 설정
@@ -43,16 +38,5 @@ public class RedisConfig {
         return template;
     }
 
-    @Bean
-    public GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer() {
-        ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json()
-                .modules(new JavaTimeModule())
-                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .build();
-
-        objectMapper.configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false);
-
-        return new GenericJackson2JsonRedisSerializer(objectMapper);
-    }
 }
 
