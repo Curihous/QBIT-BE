3c996786a2ff4b2055a9dc62d3add133233637e0
jud1thdev <8w8u8@naver.com>
2025-11-13 16:55:47 +0900

    fix: 바이낸스 응답 디코딩 ResponseEntityDecoder 사용 & add: 바이낸스 요청 필터링

--- qbit-infra/src/main/java/com/curihous/qbit/infra/binance/config/BinanceFeignConfig.java
+++ qbit-infra/src/main/java/com/curihous/qbit/infra/binance/config/BinanceFeignConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
 import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,6 +37,19 @@ public class BinanceFeignConfig {
     @Bean("binanceRequestInterceptor")
     public RequestInterceptor requestInterceptor() {
         return requestTemplate -> {
+            // binance-api 클라이언트에만 적용
+            try {
+                Object feignTarget = requestTemplate.feignTarget();
+                if (feignTarget != null) {
+                    String targetName = feignTarget.toString();
+                    if (!targetName.contains("binance-api")) {
+                        return;
+                    }
+                }
+            } catch (Exception e) {
+                // FeignTarget 정보를 가져올 수 없으면 필터링하지 않음
+            }
+            
             Set<String> allowed = Set.of("symbol", "interval", "startTime", "endTime", "limit");
             
             Map<String, Collection<String>> queries = new HashMap<>(requestTemplate.queries());
@@ -63,6 +77,6 @@ public class BinanceFeignConfig {
     
     @Bean("binanceDecoder")
     public Decoder feignDecoder() {
-        return new SpringDecoder(messageConverters);
+        return new ResponseEntityDecoder(new SpringDecoder(messageConverters));
     }
 }
